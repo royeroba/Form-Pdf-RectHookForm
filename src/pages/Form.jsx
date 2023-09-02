5a5ce0a95211d7a52cfdaa3d404d0cb0268561e7
Anibal Royero <aroyero@tvmarkets.com>
2023-09-02 00:00:09 -0500

    finish form layaut

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,18 +1,8 @@
-import SolicitudCredito from "../components/SolicitudCredito";
-import InformacionDeudor from "../components/InformacionDeudor";
-import InformacionLaboralDeudor from "../components/InformacionLaboralDeudor";
-import InformacionFinanciera from "../components/InformacionFinanciera";
-import ReferenciasDeudor from "../components/ReferenciasDeudor";
 import FormContent from "../components/FormContent";
 
 const Form = () => {
   return (
     <div className="max-w-screen-xl mx-auto mt-8 p-4 md:p-6 bg-white border rounded-lg shadow-md">
-      {/*<SolicitudCredito />
-      <InformacionDeudor />
-      <InformacionLaboralDeudor />
-      <InformacionFinanciera />
-      <ReferenciasDeudor />*/}
       <FormContent />
     </div>
   );
